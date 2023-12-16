5444bc7fcbcaf7227f005b64a78686386b1f4c85
muhaiminultasin <133908460+muhaiminultasin@users.noreply.github.com>
2023-12-17 01:21:38 +0600

    Update main.js

--- day 2 {random pss}/main.js
+++ day 2 {random pss}/main.js
@@ -2,9 +2,10 @@ const passwordBox = document.getElementById("password");
 
 function generetePass(){
     let pass = "";
+    // This is a empty variable to store the genereted password
 
     const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ + abcdefghijklmnopqrstuvwxyz~!@#$%^&*()_-+={}[]|:;<>,.?/"
-
+// it will loop 8 times to eterate the string "letter"
     for(let i = 0; i <= 8; i++){
          let char = Math.floor(Math.random() * letter.length);
 
@@ -14,10 +15,7 @@ function generetePass(){
     return passwordBox.value = pass;
 };
 
-
-
-
-
+// this function for copy the genereted password;
 function copyPass(){
     passwordBox.select();
     document.execCommand("copy");
